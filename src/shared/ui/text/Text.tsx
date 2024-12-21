8a7a05df51e58dee65688dee3371f9f23039c5e0
Danil Efremov <danileph@bk.ru>
2024-12-22 01:21:11 +0300

    feat(shared): add Text ui

---
+++ src/shared/ui/text/Text.tsx
@@ -0,0 +1,28 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import React, { forwardRef } from 'react';
+import { cn } from '@/shared/lib/tailwindUtils';
+
+const textVariants = cva('', {
+  variants: {
+    variant: {
+      default: 'leading-7 [&:not(:first-child)]:mt-6',
+      muted: 'text-sm text-muted-foreground',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof textVariants> {}
+
+/**
+ * Reusable Text Component with forwarded ref
+ * @param {TextProps} props - The props for the Text component
+ * @returns JSX.Element
+ */
+export const Text = forwardRef<HTMLParagraphElement, TextProps>(({ className, variant, ...props }, ref) => {
+  return <p ref={ref} className={cn(textVariants({ variant, className }))} {...props} />;
+});
+
+Text.displayName = 'Text';
